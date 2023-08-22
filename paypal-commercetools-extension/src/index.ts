8dffcdfb3eaaba03d874fd7012da1054d2c646dc
mbe <martin.beckmann@mediaopt.de>
2023-08-22 21:51:05 +0200

    #34704 renaming service into paypal-commercetools-extension

--- paypal-commercetools-extension/src/index.ts
+++ paypal-commercetools-extension/src/index.ts
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define routes
-app.use('/service', ServiceRoutes);
+app.use('/paypal-commercetools-extension', ServiceRoutes);
 
 // Global error handler
 app.use(errorMiddleware);
