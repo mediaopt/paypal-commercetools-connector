39bfe45f777c2547067e452fa4ae9705b1113f4b
mbe <martin.beckmann@mediaopt.de>
2023-11-16 11:46:57 +0100

    #35095 event app

--- paypal-commercetools-events/src/routes/event.route.ts
+++ paypal-commercetools-events/src/routes/event.route.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
 
-import { logger } from '../utils/logger.utils';
+import { post } from '../controllers/event.controller';
 
 const eventRouter: Router = Router();
 
-eventRouter.post('/', async (req, res) => {
-  logger.info('Event message received');
-  res.status(200);
-  res.send();
-});
+eventRouter.post('/', post);
 
 export default eventRouter;
