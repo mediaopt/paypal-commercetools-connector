3633babe7ded30ba6103b8247d49de820d037249
mbe <martin.beckmann@mediaopt.de>
2023-11-29 20:50:16 +0100

    #35096 unit tests

--- paypal-commercetools-extension/jest.config.cjs
+++ paypal-commercetools-extension/jest.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
-  displayName: 'Tests Typescript Application - Service',
+  displayName: 'Tests Typescript Application - paypal-commercetools-extension',
+  setupFiles: ["<rootDir>/tests/setup-tests.ts"],
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/tests/setup-tests.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  "collectCoverageFrom": [
+    "src/{!(paypal),}/*"
+  ],
+  coverageReporters: ["json", "html"]
 };
---
+++ paypal-commercetools-extension/tests/map.utils.spec.ts
@@ -0,0 +1,25 @@
+import { describe, test } from '@jest/globals';
+import { mapPayPalPaymentSourceToCommercetoolsMethodInfo } from '../src/utils/map.utils';
+describe('Testing map utilities', () => {
+  test.each([
+    { name: 'card', source: { card: { name: 'TEST' } } },
+    { name: 'eps', source: { eps: { name: 'TEST' } } },
+    {
+      name: 'bancontact',
+      source: { bancontact: { card_last_digits: 'TEST' } },
+    },
+    { name: 'blik', source: { blik: { name: 'TEST' } } },
+    { name: 'p24', source: { p24: { email: 'TEST' } } },
+    { name: 'giropay', source: { giropay: { name: 'TEST' } } },
+    { name: 'ideal', source: { ideal: { iban_last_chars: 'TEST' } } },
+    { name: 'mybank', source: { mybank: { iban_last_chars: 'TEST' } } },
+    { name: 'paypal', source: { paypal: { email_address: 'TEST' } } },
+    { name: 'sofort', source: { sofort: { iban_last_chars: 'TEST' } } },
+    { name: 'trustly', source: { trustly: { iban_last_chars: 'TEST' } } },
+    { name: 'venmo', source: { venmo: { email_address: 'TEST' } } },
+  ])('map $name', async ({ name, source }) => {
+    const mapped = mapPayPalPaymentSourceToCommercetoolsMethodInfo(source);
+    expect(mapped.toLowerCase()).toContain(name);
+    expect(mapped).toContain('TEST');
+  });
+});
