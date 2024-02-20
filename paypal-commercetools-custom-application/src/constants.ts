5e8649744eda313d6cd087d3eb509470ed3b546b
liudmyla <liudmyla.masliuk@mediaopt.de>
2024-02-20 12:48:33 +0100

    35821 - added custom env variable for URI path

--- paypal-commercetools-custom-application/src/constants.ts
+++ paypal-commercetools-custom-application/src/constants.ts
@@ -1,7 +1,7 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'paypal-payment-panel';
+export const entryPointUriPath = '${env:ENTRY_POINT_URI_PATH}';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
 
