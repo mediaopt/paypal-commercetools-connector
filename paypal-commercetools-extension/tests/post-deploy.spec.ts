e2097d3e37901152ebe4cd3089516dbe57b9db59
liudmyla <liudmyla.masliuk@mediaopt.de>
2024-06-10 12:27:05 +0200

    Revert "36574 - moved api mock inside test"
    This reverts commit ad06ec6056688cb2c51c42bde7426015deac4d82.

--- paypal-commercetools-extension/tests/post-deploy.spec.ts
+++ paypal-commercetools-extension/tests/post-deploy.spec.ts
@@ -1,25 +1,28 @@
 import { describe, expect } from '@jest/globals';
-import { logger } from '../src/utils/logger.utils';
 
-const apiRequest = {
-  execute: jest.fn(() => ({ body: { results: [] } })),
+let apiRequest: any = undefined;
+let apiRoot: any = undefined;
+const mockConfigModule = () => {
+  apiRequest = {
+    execute: jest.fn(() => ({ body: { results: [] } })),
+  };
+  apiRoot = {
+    extensions: jest.fn(() => apiRoot),
+    types: jest.fn(() => apiRoot),
+    customObjects: jest.fn(() => apiRoot),
+    withKey: jest.fn(() => apiRoot),
+    withContainerAndKey: jest.fn(() => apiRoot),
+    delete: jest.fn(() => apiRequest),
+    get: jest.fn(() => apiRequest),
+    post: jest.fn(() => apiRequest),
+  };
+  jest.mock('../src/client/create.client', () => {
+    return {
+      createApiRoot: () => apiRoot,
+    };
+  });
 };
-
-const apiRoot: any = {
-  extensions: jest.fn(() => apiRoot),
-  types: jest.fn(() => apiRoot),
-  customObjects: jest.fn(() => apiRoot),
-  withKey: jest.fn(() => apiRoot),
-  withContainerAndKey: jest.fn(() => apiRoot),
-  delete: jest.fn(() => apiRequest),
-  get: jest.fn(() => apiRequest),
-  post: jest.fn(() => apiRequest),
-};
-
-const webhooksApi = {
-  webhooksList: jest.fn(() => ({ data: { webhooks: [] } })),
-  webhooksPost: jest.fn(() => ({ data: { id: 1 } })),
-};
+mockConfigModule();
 
 function sleep(milliseconds: number) {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -27,17 +30,15 @@ function sleep(milliseconds: number) {
 
 describe('Testing post deploy', () => {
   test('Testing post deploy', async () => {
+    const webhooksApi = {
+      webhooksList: jest.fn(() => ({ data: { webhooks: [] } })),
+      webhooksPost: jest.fn(() => ({ data: { id: 1 } })),
+    };
     jest.mock('../src/paypal/webhooks_api', () => ({
       WebhooksApi: jest.fn().mockImplementation(() => webhooksApi),
     }));
-    jest.mock('../src/client/create.client', () => {
-      return {
-        createApiRoot: () => apiRoot,
-      };
-    });
     require('../src/connector/post-deploy');
     await sleep(19000);
-    logger.info(JSON.stringify(apiRoot.post));
     expect(webhooksApi.webhooksList).toBeCalledTimes(1);
     expect(webhooksApi.webhooksPost).toBeCalledTimes(1);
     expect(apiRoot.post).toBeCalledTimes(8);
