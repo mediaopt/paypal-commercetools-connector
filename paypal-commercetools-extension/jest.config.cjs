dbc3d5eefc3758506c118f64d877f12f1fc9c6d0
liudmyla <liudmyla.masliuk@mediaopt.de>
2024-12-05 15:33:59 +0100

    37526 - forced tests to ignore constants

--- paypal-commercetools-extension/jest.config.cjs
+++ paypal-commercetools-extension/jest.config.cjs
@@ -1,12 +1,10 @@
 module.exports = {
   displayName: 'Tests Typescript Application - paypal-commercetools-extension',
-  setupFiles: ["<rootDir>/tests/setup-tests.ts"],
+  setupFiles: ['<rootDir>/tests/setup-tests.ts'],
   moduleDirectories: ['node_modules', 'src'],
   testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  testPathIgnorePatterns: ['<rootDir>/tests/setup-tests.ts'],
+  testPathIgnorePatterns: ['<rootDir>/tests/setup-tests.ts', 'constants.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  "collectCoverageFrom": [
-    "src/{!(paypal),}/*"
-  ]
+  collectCoverageFrom: ['src/{!(paypal),}/*'],
 };
