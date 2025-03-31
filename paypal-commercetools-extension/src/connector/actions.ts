f303e553bcb44ca54493a699ea0fb4e523f404d6
liudmyla <liudmyla.masliuk@mediaopt.de>
2025-03-31 13:26:25 +0200

    37928 - added some logs

--- paypal-commercetools-extension/src/connector/actions.ts
+++ paypal-commercetools-extension/src/connector/actions.ts
@@ -14,6 +14,7 @@ import {
   deleteAccessToken,
   getCachedAccessToken,
 } from '../service/config.service';
+import { logger } from '../utils/logger.utils';
 
 export const PAYPAL_PAYMENT_EXTENSION_KEY = 'paypal-payment-extension';
 export const PAYPAL_CUSTOMER_EXTENSION_KEY = 'paypal-customer-extension';
@@ -425,6 +426,11 @@ export async function createAndSetCustomObject(
 export const deleteAccessTokenIfExists = async (
   lastTenantType?: 'single' | 'multi'
 ) => {
+  logger.info(
+    `Deleting access token(s) for ${
+      lastTenantType ?? 'any kind of'
+    } tenant store if exists`
+  );
   if (lastTenantType === 'single' || !lastTenantType) {
     if (await getCachedAccessToken()) {
       await deleteAccessToken();
