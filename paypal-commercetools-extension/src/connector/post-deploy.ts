af5b09d31497b38ef7fb9f6523826e6f1e81b9e2
liudmyla <liudmyla.masliuk@mediaopt.de>
2025-03-31 11:14:48 +0200

    37928 - added multi tenant option to create webhook in post deploy

--- paypal-commercetools-extension/src/connector/post-deploy.ts
+++ paypal-commercetools-extension/src/connector/post-deploy.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { createApiRoot } from '../client/create.client';
-import { createWebhook } from '../service/paypal.service';
+import { createWebhook, createWebhooks } from '../service/paypal.service';
 import { assertError, assertString } from '../utils/assert.utils';
 import { readConfiguration } from '../utils/config.utils';
 import {
@@ -16,20 +16,23 @@ import {
 } from './actions';
 
 const CONNECT_APPLICATION_URL_KEY = 'CONNECT_SERVICE_URL';
+const MULTI_TENANT_IDS = 'PAYPAL_MULTI_TENANT_CLIENT_IDS';
 
 async function postDeploy(properties: Map<string, unknown>): Promise<void> {
   const applicationUrl = properties.get(CONNECT_APPLICATION_URL_KEY);
+  const isMultiTenant = !!properties.get(MULTI_TENANT_IDS);
 
   assertString(applicationUrl, CONNECT_APPLICATION_URL_KEY);
 
-  const apiRoot = createApiRoot();
-  await deleteAccessTokenIfExists();
-  await createPaymentUpdateExtension(apiRoot, applicationUrl);
-  await createCustomerUpdateExtension(apiRoot, applicationUrl);
-  await createCustomPaymentType(apiRoot);
-  await createCustomCustomerType(apiRoot);
-  await createCustomPaymentInteractionType(apiRoot);
-  await createWebhook();
+  const apiRoot = createApiRoot(); //doesn't involve multi tenant, is commercetools global
+  await deleteAccessTokenIfExists(); //already accounts for multi tenant
+  await createPaymentUpdateExtension(apiRoot, applicationUrl); //doesn't involve multi tenant directly, only need to pass store key if relevant
+  await createCustomerUpdateExtension(apiRoot, applicationUrl); //doesn't involve multi tenant directly, only need to pass store key if relevant
+  await createCustomPaymentType(apiRoot); //doesn't involve multi tenant directly, only need to pass store key if relevant
+  await createCustomCustomerType(apiRoot); //doesn't involve multi tenant directly, only need to pass store key if relevant
+  await createCustomPaymentInteractionType(apiRoot); //doesn't involve multi tenant directly, only need to pass store key if relevant
+  if (isMultiTenant) await createWebhooks();
+  else await createWebhook();
   await createAndSetCustomObject(apiRoot);
 }
 
