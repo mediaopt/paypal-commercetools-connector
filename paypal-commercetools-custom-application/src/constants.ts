82523142dee67cfcadcea73f781cf3d1b610f434
liudmyla <liudmyla.masliuk@mediaopt.de>
2025-07-01 12:18:38 +0200

    38375 - updated constants specifications to match typescript requirements and mc app latest template

--- paypal-commercetools-custom-application/src/constants.ts
+++ paypal-commercetools-custom-application/src/constants.ts
@@ -1,1 +1,13 @@
-export * from './constants.mjs';
+// Make sure to import the helper functions from the `ssr` entry point.
+import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
+
+export const entryPointUriPath =
+  typeof window === 'undefined'
+    ? process.env.ENTRY_POINT_URI_PATH
+    : (window as any).app.entryPointUriPath ?? 'paypal-payment-panel';
+export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
+
+export const GRAPHQL_CUSTOMOBJECT_CONTAINER_NAME =
+  'paypal-commercetools-connector';
+
+export const GRAPHQL_CUSTOMOBJECT_KEY_NAME = 'settings';
